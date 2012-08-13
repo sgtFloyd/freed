fc5cd57202bc61da6fd43eb5bde2bcfce0045be1
Gabe Smith <gabe@paperlesspost.com>
2012-08-13 19:06:56 -0400

    Basic security for authorizing email verification and feed removal

--- app/freed.rb
+++ app/freed.rb
@@ -59,11 +59,23 @@ def update_feed(id)
   end
 end
 
-def delete_feed(id)
+def delete_feed(id, sig)
+  if sig == feed_signature(id)
+    redis = settings.redis
+    if redis.sismember('freed:feeds', id)
+      redis.srem("freed:feeds", id)
+      redis.del("freed:#{id}")
+    end
+  else
+    send_delete_verification_email(id)
+  end
+end
+
+def verify_feed(id, sig)
+  return unless sig == feed_signature(id)
   redis = settings.redis
   if redis.sismember('freed:feeds', id)
-    redis.srem("freed:feeds", id)
-    redis.del("freed:#{id}")
+    redis.hset("freed:#{id}", 'email_verified', true)
   end
 end
 
@@ -74,11 +86,23 @@ def all_feeds
   end.sort_by{|id, feed| -feed['last_checked'].to_i}]
 end
 
+def feed_signature(id)
+  Digest::SHA1.hexdigest(id + settings.secret_hash_key)
+end
+
 # ============================== EMAILS ============================== #
 
 def send_email_verification(recipient, feed_id, feed_url)
+  feed_sig = feed_signature(feed_id)
   send_email( recipient, 'verify_email',
-    {feed_id: feed_id, feed_url: feed_url})
+    {feed_id: feed_id, feed_url: feed_url, feed_sig: feed_sig} )
+end
+
+def send_delete_verification_email(feed_id)
+  feed = settings.redis.hgetall("freed:#{feed_id}")
+  feed_sig = feed_signature(feed_id)
+  send_email(feed['notify_email'], 'verify_delete',
+    {feed_id: feed_id, feed_url: feed['feed_url'], feed_sig: feed_sig} )
 end
 
 def send_update_email(feed_id, changes)
@@ -96,6 +120,7 @@ def send_email(recipient, template, locals)
     smtp.send_message(content, from, recipient)
   end
 rescue => e
+  puts "Caught: #{e}"
 end
 
 # ============================== SINATRA ============================== #
@@ -107,7 +132,8 @@ configure do
                             :port => redistogo.port,
                             :password => redistogo.password)
     set :gmail_user => conf[:gmail_user],
-        :gmail_pass => conf[:gmail_pass]
+        :gmail_pass => conf[:gmail_pass],
+        :secret_hash_key => conf[:secret_hash_key]
   end
 end
 
@@ -128,13 +154,16 @@ end
 
 # DELETE
 delete '/feed/:id' do
-  delete_feed params[:id]
+  delete_feed params[:id], params[:sig]
 end
 
+
 get '/feed/verify/:id' do
-  redis = settings.redis
-  if redis.sismember('freed:feeds', params[:id])
-    redis.hset("freed:#{params[:id]}", 'email_verified', true)
-  end
+  verify_feed params[:id], params[:sig]
+  redirect '/'
+end
+
+get '/feed/delete/:id' do
+  delete_feed params[:id], params[:sig]
   redirect '/'
 end
