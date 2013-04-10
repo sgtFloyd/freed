368347c0466a7caf0f2ffc23d76d5d76c63d3d43
Gabe <gabe@paperlesspost.com>
2013-04-10 18:03:14 -0400

    separate static files

---
+++ app/public/freed.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  // delete feeds
+  $('.delete').click(function(){
+    var feed_id = $(this).closest('tr').attr('id');
+    $.ajax({
+      url: '/feed/' + feed_id,
+      type: 'DELETE',
+      success: fade(feed_id)
+    });
+  });
+
+  // show delete link on hover
+  $('tr').hover(
+    function(){ $(this).find('.delete').show(); },
+    function(){ $(this).find('.delete').hide(); }
+  );
+
+  var fade = function(feed_id) {
+    $('tbody').append(
+      $('#'+feed_id).addClass('faded')
+        .find('.delete').remove().end()
+    );
+    alert('An email has been sent to '+
+      $('#'+feed_id+' .email').text()+
+      ' to confirm deletion.');
+  };
+});
